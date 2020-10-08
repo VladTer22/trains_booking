1b81ee6b58fd60d950b847b3d8f6cfdf799ce6dd
VladTer22 <volodymyrtereshchuk22@gmail.com>
2020-10-08 11:24:20 +0300

    Rorbasic Lesson12 NotesFixes

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,4 @@
 class RailwayStationRoute < ApplicationRecord
-  belongs_to :railway_station
-  belongs_to :route
+  belongs_to :railway_station, dependent: :destroy
+  belongs_to :route, dependent: :destroy
 end
