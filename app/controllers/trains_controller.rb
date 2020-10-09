057ec2172faa889173db6ba68eb0f40e3c4bfb9a
VladTer22 <volodymyrtereshchuk22@gmail.com>
2020-10-09 10:05:30 +0300

    Rorbasic Lesson13 Fixed

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,26 +1,18 @@
 class TrainsController < ApplicationController
   before_action :set_train, only: %i[show edit update destroy]
 
-  # GET /trains
-  # GET /trains.json
   def index
     @trains = Train.all
   end
 
-  # GET /trains/1
-  # GET /trains/1.json
   def show; end
 
-  # GET /trains/new
   def new
     @train = Train.new
   end
 
-  # GET /trains/1/edit
   def edit; end
 
-  # POST /trains
-  # POST /trains.json
   def create
     @train = Train.new(train_params)
 
@@ -35,8 +27,6 @@ class TrainsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /trains/1
-  # PATCH/PUT /trains/1.json
   def update
     respond_to do |format|
       if @train.update(train_params)
@@ -49,8 +39,6 @@ class TrainsController < ApplicationController
     end
   end
 
-  # DELETE /trains/1
-  # DELETE /trains/1.json
   def destroy
     @train.destroy
     respond_to do |format|
@@ -61,12 +49,10 @@ class TrainsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_train
     @train = Train.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def train_params
     params.require(:train).permit(:number, :route_id)
   end
